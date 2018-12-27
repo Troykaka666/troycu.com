c39c8a8f657e68b3e5e08744faef5703955a9448
Troy <Troywu9527@gmail.com>
2018-12-27 17:07:08 -0500

    set up gulpfile for the scss

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefix = require('gulp-autoprefixer'),
+    sass = require("gulp-sass"),
+    sourcemap = require('gulp-sourcemaps')
+    cssmin = require('gulp-cssnano'),
+    rename = require('gulp-rename');
+
+    var prefixerOptions = {
+        browsers: ["last 2 versions"]
+      };
+
+gulp.task("style", function(){
+    return gulp
+        .src("./Temp/style.scss")
+        .pipe(sourcemap.init())
+        .pipe(
+            sass({ includePaths: require('node-normalize-scss').includePaths }).on(
+                "error",
+                sass.logError
+            )
+        )
+        .pipe(prefix(prefixerOptions))
+        .on("error", function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit("end");
+        })
+        .pipe(rename("newstyle.css"))
+        .pipe(gulp.dest("./new_css/"))
+        .pipe(cssmin())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("./css_new/"));
+});
+
+gulp.task("watch", function(){
+    gulp.watch("./Temp/**/*.scss", gulp.series('style'));
+});
